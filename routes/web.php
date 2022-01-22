de4cbe060d4b7a5567cd2a4b290e8fc6608dac9d
zakkysurya <zakkysurya@gmail.com>
2022-01-22 07:20:29 +0700

    frist templating - focus 2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('layouts.base');
 });
